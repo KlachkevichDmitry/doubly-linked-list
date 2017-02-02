3378de52089924f3046a741d58af033876e235c6
dklachkevich <klachkevich.dmitry@gmail.com>
2017-02-03 01:15:54 +0400

    9 14

--- src/linked-list.js
+++ src/linked-list.js
@@ -5,112 +5,77 @@ class LinkedList {
         this._head=null;
         this._tail=null;
         this.length=0;
-
-
     }
 
 
-     append(data) {  /* добавление новой ячейки, при добавлении в пустой список присваивать значения добавляемой ячейки,
+    append(data) {  /* добавление новой ячейки, при добавлении в пустой список присваивать значения добавляемой ячейки,
        при добавлении не в пустой список добавлять данные в конец списка*/
-
            var newpart=new Node(data);
            if (this.length==0) {
              this._head=newpart;
              this._tail=newpart;
            } else {
                this._tail.next=newpart;
-               newpart.prev=this.tail;
+               newpart.prev=this._tail;
                this._tail=newpart;
-
            }
          this.length++;
          return this;
-
-
-
-
     }
 
-   head() {  /* начало списка */
-
+    head() {  /* начало списка */
        if (this.length != null) {
             return this._head.data;
         }
-
     }
 
      tail() {  /* конец списка */
          if (this.length != null) {
              return this._tail.data;
          }
-
      }
 
     at(index) { /*возвращает данные по индексу*/
-
         var fact=this._head;
-
         for (var i=0; i<index; i++) {
             fact=fact.next;
         }
         return fact.data;
-
     }
 
     insertAt(index, data) { /*вставляет данные в определенный индекс*/
-        
-
+        var fact=this._head;
+        for (var i=0; i<index; i++)  {
+            fact=fact.next;
+            if (i=index){
+            fact.data=(new Node(data)).data;
+            }
+        }
     }
 
     isEmpty() {  /* проверка на наличие данных в списке */
-
          if (this.length==0) {
          return true;
          } else {
              return false;
-
          }
-
        }
 
     clear()  {  /*удаляет данные из списка*/
-      
-        while (this.length>0) {
+            while (this.length>0) {
             this._head.data = null;
             this._head.prev = null;
             this._head.next = null;
             this._tail.data=null;
             this._tail.prev = null;
             this._tail.next = null;
-
             this.length--;
         }
-
-
-
-       
-
-
-
-        /*if (this.length>0) {
-         this._head.next=null;
-         this._tail.prev=null;
-         this._tail.=null;
-         this._head=null;
-         this.length--;
-         }*/
-
-        /* this._head=null;
-         this._tail=null;
-         this.length=0;
-         return this; */
-
     }
 
 
     deleteAt(index) { /* удаляет элемент по индексу*/
 
-        }
 
     reverse() { /*меняет направление списка */
         }
